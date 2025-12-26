fc17601902e10751aa502729ad38ec20439b4630
Tal Metzger <talmetzger3011@gmail.com>
2025-12-26 14:20:13 +0200

    small update to subscriber class

--- G2_Assignment3/Common/src/logic/Subscriber.java
+++ G2_Assignment3/Common/src/logic/Subscriber.java
@@ -4,21 +4,28 @@ public class Subscriber {
 
 	private Integer subscriberId; // DB-generated
 	private String username;
+	private String firstName;
+	private String lastName;
 	private String email;
 	private String phone;
 	private String passwordHash;
 
-	public Subscriber(String username, String email, String phone) {
+	public Subscriber(String username, String firstName, String lastName, String email, String phone) {
 		this.subscriberId = null;
 		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.email = email;
 		this.phone = phone;
-		this.passwordHash = null; 
+		this.passwordHash = null;
 	}
 
-	public Subscriber(Integer subscriberId, String username, String email, String phone, String passwordHash) {
+	public Subscriber(Integer subscriberId, String username, String firstName, String lastName, String email,
+			String phone, String passwordHash) {
 		this.subscriberId = subscriberId;
 		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.email = email;
 		this.phone = phone;
 		this.passwordHash = passwordHash;
@@ -38,6 +45,22 @@ public class Subscriber {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public String getEmail() {
