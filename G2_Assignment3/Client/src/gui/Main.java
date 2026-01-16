08bf81114227bafbb2470004c144584c012d019e
liraztubul <liraz.t41@gmail.com>
2026-01-16 22:51:57 +0200

    css

--- G2_Assignment3/Client/src/gui/Main.java
+++ G2_Assignment3/Client/src/gui/Main.java
@@ -55,8 +55,9 @@ public class Main extends Application {
 		// Initialize the client connection with host name and default port.
 		client = ClientController.getInstance(host, ClientController.DEFAULT_PORT);
 		// Set up the scene and display the stage
-		Scene s = new Scene(root);
+		Scene s = new Scene(root, 600, 500);
 		primaryStage.setScene(s);
+
 		primaryStage.setOnCloseRequest(event ->{
 			client.quit();
 			System.exit(0);
@@ -84,6 +85,7 @@ public class Main extends Application {
 	    Parent newRoot = FXMLLoader.load(Main.class.getResource(fxmlName));
 	    primaryStage.getScene().setRoot(newRoot);
 	}
+
 	public static void changeRoot(String fxmlName, int width, int height) throws IOException {
         Parent newRoot = FXMLLoader.load(Main.class.getResource(fxmlName));
         primaryStage.getScene().setRoot(newRoot);
